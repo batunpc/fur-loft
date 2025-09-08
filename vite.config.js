5a000816b685d7ebac0dfccc4f018eac9b7ddd2e
batunpc <batuhnpc@gmail.com>
2025-09-08 18:45:15 -0400

    fixes

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from "vite";
-import { resolve } from "path";
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   server: {
@@ -9,19 +9,16 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "index.html"),
-        about: resolve(__dirname, "about.html"),
-        work: resolve(__dirname, "work.html"),
-        project: resolve(__dirname, "project.html"),
-        contact: resolve(__dirname, "contact.html"),
+        main: resolve(__dirname, 'index.html'),
+        contact: resolve(__dirname, 'contact.html'),
       },
     },
     assetsInclude: [
-      "**/*.jpeg",
-      "**/*.jpg",
-      "**/*.png",
-      "**/*.svg",
-      "**/*.gif",
+      '**/*.jpeg',
+      '**/*.jpg',
+      '**/*.png',
+      '**/*.svg',
+      '**/*.gif',
     ],
     copyPublicDir: true,
   },
